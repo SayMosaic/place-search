d9b2a9ff86f8ca4c1ca695fc3c9cb631fcca621e
Xuezheng Ma <xuezheng-ma@uiowa.edu>
2018-02-02 17:38:27 -0800

    Add google text, and nearby, APIs, and first real test case

--- test/index.js
+++ test/index.js
@@ -1,15 +1,24 @@
 const assert = require('assert')
 const placeSearch = require('../lib').placeSearch
 
+const testCoordinates = {latitude: '37.43038850000001', longitude: '-122.1008473'}
 
 const tests = [
   {
     destination: '!#@!#@!#@!#@!#',
-    area: '',
-    coordinates: {latitude: '', longitude: ''},
+    area: null,
+    coordinates: testCoordinates,
     radius: 50000,
-    output: [],
+    verifyOutput: res => assert.deepEqual(res, []),
     description: 'Should return an empty array when nothing can be found'
+  },
+  {
+    destination: 'starbucks',
+    area: null,
+    coordinates: testCoordinates,
+    radius: null,
+    verifyOutput: res => assert.equal(res[0].place_id, 'ChIJT_9zPBC6j4ARm2Fu2gYDC0k'),
+    description: 'Should find the coffee shop at 2410 Charleston Rd, Mountain View when I am at 1121 San Antonio Road Palo Alto '
   }
 ]
 
@@ -17,9 +26,12 @@ const tests = [
 
 describe('.placeSearch()', function placeSearchTest() {
   tests.forEach(test => {
-    const {destination, area, coordinates, radius} = test
-    it(test.description, function nothingToBeFound() {
-      assert.deepEqual(placeSearch(destination, area, coordinates, radius), [])
+    const {destination, area, coordinates, radius, verifyOutput} = test
+    it(test.description, function nothingToBeFound(done) {
+      placeSearch(destination, area, coordinates, radius)
+        .then(verifyOutput)
+        .then(done)
+        .catch(done)
     })
   })
 
